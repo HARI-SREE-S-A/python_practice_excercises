d742ab1be6054f4c24891d1b8533ab1c6c065350
HARISREE SURESH <getharisree@outlook.com>
2023-01-23 18:53:36 +0530

    Update budget_allocation.py

--- budget_allocation.py
+++ budget_allocation.py
@@ -2,10 +2,21 @@
 nums = [1,2,3,4,5,6]
 target = 9
 new = sorted(nums)
+list = []
 
 for i,n in enumerate(nums):
 	l = 1
 	r = len(nums)-1
 	if i > 0 and n == new[i-1]:
+		continue
+	while l < r:
+		summ = n + new[l] + new[r]
+		if summ > target:
+			l -= 1
+		elif summ < target:
+			r += 1
+		else:
+			list.append([n,new[l],new[r]])
+			
 	
   
