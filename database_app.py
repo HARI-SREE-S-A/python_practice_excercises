70c7d2fa7bea93d165db1d0e429f7782f058646e
HARISREE SURESH <getharisree@outlook.com>
2023-01-30 19:48:06 +0530

    Update database_app.py

--- database_app.py
+++ database_app.py
@@ -1,1 +1,22 @@
+import sqlite3
+
+id_n = input("enter thew id number")
+name = input("enter your name")
+desig = input("enter the designation")
+
+
+
+
+initializing = sqlite3.connect("sample_database.db")
+
+cur = initializing.cursor()
+
+
+#creating the table in database
+
+
+tab = """CREATE TABLE IF NOT EXISTS notes(id INTEGER PRIMARYKEY,name TEXT,designation TEXT);'''
+cur.execute(tab)
+
+
 
