b2c6f7d1db8403d9bf45305b9e6cc24f1bfe0500
HARISREE SURESH <getharisree@outlook.com>
2023-01-30 17:19:52 +0530

    Update creating_and_linking_database.py

--- creating_and_linking_database.py
+++ creating_and_linking_database.py
@@ -1,1 +1,12 @@
+import sqlite3
+
+try:
+
+  connect = sqlite3.connect("database.db")
+  print("connected")
+except sqlite3.Error as error:
+  print("not connected",error)
+
+finally:
+  connect.dis
 
