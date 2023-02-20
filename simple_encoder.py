7d094714472cf5c76b5b2ea9040329d2bcb52e73
HARISREE SURESH <getharisree@outlook.com>
2023-02-20 19:50:04 +0530

    Update simple_encoder.py

--- simple_encoder.py
+++ simple_encoder.py
@@ -1,3 +1,8 @@
 #should return an alphabet which is four more than its original index for  character given
-ref = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-l = []
+def encrypt(c):
+  ref = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  l = []
+  for w in c:
+    n = ref.index(w)
+    s = n + 4
+
