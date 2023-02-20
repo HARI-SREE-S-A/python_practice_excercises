9b472b5afe2a2d64a36563207adc4348c82067d9
HARISREE SURESH <getharisree@outlook.com>
2023-02-20 19:47:26 +0530

    Update simple_encoder.py

--- simple_encoder.py
+++ simple_encoder.py
@@ -1,1 +1,1 @@
-#should return an alphabet which is four more than its original index for  caharcter given
+#should return an alphabet which is four more than its original index for  character given
