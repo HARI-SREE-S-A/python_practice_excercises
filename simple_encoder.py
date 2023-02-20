582c0c41997600929f2b5cb9b8825cb8a990a88c
HARISREE SURESH <getharisree@outlook.com>
2023-02-20 19:48:13 +0530

    Update simple_encoder.py

--- simple_encoder.py
+++ simple_encoder.py
@@ -1,1 +1,3 @@
 #should return an alphabet which is four more than its original index for  character given
+ref = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+l = []
