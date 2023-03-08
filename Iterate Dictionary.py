aa3805efc6cc4925db9504a559de52570b367709
HARISREE SURESH <getharisree@outlook.com>
2023-03-08 14:26:01 +0530

    Update Iterate Dictionary.py

--- Iterate Dictionary.py
+++ Iterate Dictionary.py
@@ -1,2 +1,10 @@
 d = dict(a = list(range(1, 11)), b = list(range(11, 21)), c = list(range(21, 31)))
 print("a has the quantity of  ",d["a"])
+
+
+
+#############################################
+
+d = dict(a = list(range(1, 11)), b = list(range(11, 21)), c = list(range(21, 31)))
+for key,values in d.items():
+    print(key,"has items",values)
