696fe290bec82a3395fbb2d3f692ec5c315ee2be
HARISREE SURESH <getharisree@outlook.com>
2023-03-08 14:35:33 +0530

    Update Print_Alphabet.py

--- Print_Alphabet.py
+++ Print_Alphabet.py
@@ -1,3 +1,12 @@
 import string
 for letters in string.ascii_uppercase:
     print(letters)
+    
+    
+ ############# printing all alphabets as a list
+
+import string
+l = []
+for letters in string.ascii_lowercase:
+    l.append(letters)
+print(l)
