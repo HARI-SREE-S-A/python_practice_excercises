c808422f0e246d977c481be55448de1b05df1e9f
HARISREE SURESH <getharisree@outlook.com>
2023-04-18 20:07:03 +0530

    Update letter_2by2.py

--- letter_2by2.py
+++ letter_2by2.py
@@ -1,1 +1,15 @@
+import string
+l = []
+k = []
 
+with open("letters.txt","w") as file:
+    for letter in string.ascii_lowercase:
+        l.append(letter)
+        k.append(letter)
+q,o = l[::2] ,k[1::2]
+
+for i in range(0,14):
+    print(q[i]+o[i])
+  
+    
+   
