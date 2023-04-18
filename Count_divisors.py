138d4b613febc7be00e0637dc892ee081a75ceaa
HARISREE SURESH <getharisree@outlook.com>
2023-04-18 20:13:51 +0530

    Update Count_divisors.py

--- Count_divisors.py
+++ Count_divisors.py
@@ -1,8 +1,7 @@
 nums = [int(x) for x in input().split(" ")]
 count = 0
 for p in range(nums[0],nums[1]+1):
-  if p % nums[2] == 0:
-           
+  if p % nums[2] == 0:         
     
   count += 1
 return count
