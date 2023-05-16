13f40ed5c7dbf2b8391b70708fc5adbf64959f2c
HARISREE SURESH <getharisree@outlook.com>
2023-05-16 21:38:51 +0530

    Update Letters Three by Three.py

--- Letters Three by Three.py
+++ Letters Three by Three.py
@@ -1,1 +1,21 @@
+import string
+
+a = string.ascii_lowercase[::1]
+b = string.ascii_lowercase[1::3]
+c = string.ascii_lowercase[2::3]
+i = 0
+while i < 27:
+    try:
+        if i == 24:
+            print(a[24],a[25])
+            break
+        else:
+            print(a[i]+a[i+1]+a[i+2]+"\n")
+        i += 3
+    except:
+        break
+
+
+
+
 
