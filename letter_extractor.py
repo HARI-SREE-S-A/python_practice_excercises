84c7d415b5dc880e741e5ae605912db48e8c2b74
HARISREE SURESH <getharisree@outlook.com>
2023-05-17 21:29:44 +0530

    Update letter_extractor.py

--- letter_extractor.py
+++ letter_extractor.py
@@ -1,1 +1,13 @@
+import string
+nm = string.ascii_lowercase[::1]
+lisst = []
+
+for name in nm:
+
+
+    with open(name + ".txt","r") as f:
+        lisst.append(f.read().strip("\n"))
+
+
+print(lisst)
 
