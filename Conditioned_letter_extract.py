95818716146934f3d32215feccdfb0279dd92bd3
HARISREE SURESH <getharisree@outlook.com>
2023-05-17 21:54:27 +0530

    Update Conditioned_letter_extract.py

--- Conditioned_letter_extract.py
+++ Conditioned_letter_extract.py
@@ -1,1 +1,19 @@
+import string
+
+nm = string.ascii_lowercase[::1]
+lisst = []
+py = ["p", "y", "t", "h", "o", "n"]
+
+for name in nm:
+
+
+    with open(name + ".txt", "r") as f:
+        print(f.read().strip("\n"))
+        if (f.read().strip("\n")) in py:
+            print("hi")
+            lisst.append(f.read().strip("\n"))
+        else:
+            continue
+
+print(lisst)
 
