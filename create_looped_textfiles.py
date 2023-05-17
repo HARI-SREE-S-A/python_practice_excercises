c5a1fb8d5187565f48837d7f5a23bc7e7a7e22c3
HARISREE SURESH <getharisree@outlook.com>
2023-05-17 21:23:32 +0530

    Update create_looped_textfiles.py

--- create_looped_textfiles.py
+++ create_looped_textfiles.py
@@ -1,1 +1,12 @@
+import string
+def name():
 
+    lisst = string.ascii_lowercase[::1]
+    for n in lisst:
+        with open(str(n),'w') as file:
+            file.write(n)
+            file.close()
+
+
+
+name()
