95e7519720bdcd10c0208ec14a710cbf1cdc4e64
HARISREE SURESH <getharisree@outlook.com>
2023-05-22 21:22:19 +0530

    Update Valid_brackets.py

--- Valid_brackets.py
+++ Valid_brackets.py
@@ -1,1 +1,16 @@
+s = "{[(])}"
+
+d = {"]":"[",")":"("}
+
+stack  =[]
+for c in s:
+    if c in d :
+        if stack and stack[-1] == d[c]:
+            stack.pop()
+        else:
+            print(False)
+    else:
+        stack.append(c)
+print(True) if not stack else print(False)
+
 
