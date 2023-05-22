c4050880f87ed49999fb25fdb19f67a4044f9e03
HARISREE SURESH <getharisree@outlook.com>
2023-05-22 21:53:02 +0530

    Update finding_maxSum.py

--- finding_maxSum.py
+++ finding_maxSum.py
@@ -1,1 +1,10 @@
+nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 
+curr = nums[0]
+maxm = nums[0]
+
+for i in range(1,len(nums)):
+    curr = max(nums[i],curr + nums[i])
+    maxm = max(maxm,curr)
+
+print(maxm)
