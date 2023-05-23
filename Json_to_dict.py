4793f752a8bf9da54ad464d0716818d76419fec8
HARISREE SURESH <getharisree@outlook.com>
2023-05-23 21:47:42 +0530

    Update Json_to_dict.py

--- Json_to_dict.py
+++ Json_to_dict.py
@@ -2,3 +2,11 @@ with open("testjason.json","r") as f:
     b = f.read()
     print(b)
     f.close()
+ ##################################
+
+import json
+import pprint
+with open("company1.json","r") as f:
+    b = json.loads(f.read())
+    print(b)
+    f.close()
