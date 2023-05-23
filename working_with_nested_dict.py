5abdc06c40b4dd15efa911c1769b26cabf6d4b1d
HARISREE SURESH <getharisree@outlook.com>
2023-05-23 21:20:23 +0530

    Update working_with_nested_dict.py

--- working_with_nested_dict.py
+++ working_with_nested_dict.py
@@ -5,3 +5,15 @@ d = {"employees":[{"firstName": "John", "lastName": "Doe"},
           {"firstName": "Jessy", "lastName": "Petter"}]}
 
 print(d["employees"][1]["lastName"])
+
+
+################################################################
+d = {"employees":[{"firstName": "John", "lastName": "Doe"},
+                {"firstName": "Anna", "lastName": "Smith"},
+                {"firstName": "Peter", "lastName": "Jones"}],
+"owners":[{"firstName": "Jack", "lastName": "Petter"},
+          {"firstName": "Jessy", "lastName": "Petter"}]}
+
+(d["employees"][1]["lastName"]) = "sasi"
+
+print(d)
