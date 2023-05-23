cc86660e389e7d8219e90a254951099857d7d60a
HARISREE SURESH <getharisree@outlook.com>
2023-05-23 21:38:17 +0530

    Update creating_json.py

--- creating_json.py
+++ creating_json.py
@@ -1,1 +1,13 @@
+d = {"employees":[{"firstName": "John", "lastName": "Doe"},
+                {"firstName": "Anna", "lastName": "Smith"},
+                {"firstName": "Peter", "lastName": "Jones"}],
+"owners":[{"firstName": "Jack", "lastName": "Petter"},
+          {"firstName": "Jessy", "lastName": "Petter"}]}
 
+(d["employees"].append({"firstName":"thangu","secondname":"sasi"}))
+
+with open("testjason.json","w") as f:
+    f.write(str(d))
+    f.close()
+
+print(d)
