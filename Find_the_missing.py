91bc279c5817c1d23d9b46d68433161114b95d0e
HARISREE SURESH <getharisree@outlook.com>
2023-06-06 18:50:04 +0530

    Update Find_the_missing.py

--- Find_the_missing.py
+++ Find_the_missing.py
@@ -1,1 +1,10 @@
+# finding the missing number
+
+n = [int(c) for c in input("enter the number with space").split(" ")]
+
+n = sorted(n)
+
+for x in range(n[1],n[-1]+1):
+    if x not in n:
+        print(x)
 
